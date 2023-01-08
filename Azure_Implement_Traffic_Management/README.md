8f0843381d12a11895b7e84e37b5723d0224fe4f
emineozsahin <eozsahin@uoguelph.ca>
2023-01-08 00:32:11 -0500

    updates

--- Azure_Implement_Traffic_Management/README.md
+++ Azure_Implement_Traffic_Management/README.md
@@ -1,13 +1,37 @@
-#
+# IMPLEMENT TRAFFIC MANAGEMENT IN AZURE
+
+A github account called MicrosoftLearning has a [Lab6](https://github.com/MicrosoftLearning/AZ-104-MicrosoftAzureAdministrator/blob/master/Instructions/Labs/LAB_06-Implement_Network_Traffic_Management.md) which is the source of this project. It explains each task step by step.
+
+I try to add two other ways to provision the environment:
+
+1. Azure CLI
+2. Terraform
+: under construction
+
+I followed the Microsoft Learning steps for the task 2-6.
+
+## Provision the environment
+[Provisioning](https://www.redhat.com/en/topics/automation/what-is-provisioning) is the process of setting up IT infrastructure. In this repository I write a [bash script](https://github.com/emineozsahin/Azure/blob/main/Azure_Implement_Traffic_Management/implement_traffic_management.sh) for provisioning the environment that is figured in the [project.png](https://github.com/emineozsahin/Azure/blob/main/Azure_Implement_Traffic_Management/project.png) file.
+
+```sh
+sh implement_traffic_management.sh
+```
+
+I have also used the arm templates obtained from MicrosoftLearning-Lab6 as a second method to this repository following the [instructions](https://github.com/MicrosoftLearning/AZ-104-MicrosoftAzureAdministrator/blob/master/Instructions/Labs/LAB_06-Implement_Network_Traffic_Management.md).
+
+I'll update the repository by adding terraform files as a third way to automate the provisioning.
+
+## Configure hub and spoke network topology
+hub: az104-06-vnet01
+spoke: az104-06-vnet2 and az104-06-vnet3
+
+## Test transitivity of virtual networking peering
+
+## Configure routing in the hub and spoke topology
+
+## Implement Azure Load Balancer
+
+## Implement Azure Application Gateway
 
 
-1. Provision the environment
-2. Configure hub and spoke network topology
-3. Test transitivity of virtual networking peering
-4. Configure routing in the hub and spoke topology
-5. Implement Azure Load Balancer
-6. Implement Azure Application Gateway
 
-
-
-az configure --defaults location=<location>
--- Azure_Implement_Traffic_Management/implement_traffic_management.sh
+++ Azure_Implement_Traffic_Management/implement_traffic_management.sh
@@ -1,18 +1,13 @@
 #! bin/bash
 
-for i in az104-06-rg1 az104-06-rg4 az104-06-rg5
-do
-  az group delete --name $i
-done
-
 basename="az104-06"
 location="eastus2"
 
 # Task1
-## Generate three resource groups named az104-06-rg1, az104-06-rg4, az104-06-rg6
+## Generate three resource groups named az104-06-rg1, az104-06-rg4, az104-06-rg5
 for i in rg1 rg4 rg5; do az group create -l $location -n ${basename}-$i;done
 
-## Generate 3 vnets and its subnet0 in az104-06-rg1 named az104-06-vnet01, az104-06-vnet2, az104-06-vnet3
+## Generate 3 vnets named az104-06-vnet01, az104-06-vnet2, az104-06-vnet3 and their subnet0s in az104-06-rg1
 for i in vnet01-60 vnet2-62 vnet3-63
 do
    num=`echo $i | sed 's/vnet[0-9]*-//g'`
@@ -24,7 +19,7 @@ do
    --subnet-prefixes 10.${num}.0.0/24
 done
 
-## Generate Subnet1 and Subnet-appgw in vnet01, az104-06-rg1
+## Generate Subnet1 and Subnet-appgw in vnet01
 for i in Subnet1_1.0/24 Subnet-appgw_3.224/27
 do
 name=`echo $i |sed 's/_.*//g'`
@@ -36,12 +31,11 @@ az network vnet subnet create \
   --address-prefixes 10.60.${num}
 done
 
-## Generate VMs in az104-06-rg1
+## Generate four VMs in az104-06-rg1
 for i in az104-06-vnet01_0 az104-06-vnet01_1 az104-06-vnet2_0 az104-06-vnet3_0
 do
 vnet=`echo $i| sed 's/_.*//g'`
 subnet=`echo $i| sed 's/.*_//g'`
-#echo $vnet $subnet
 az vm create \
     --resource-group az104-06-rg1 \
     --location $location \
@@ -51,38 +45,41 @@ az vm create \
     --admin-username  ${basename}-vm${subnet}\
     --vnet-name $vnet \
     --subnet Subnet${subnet} \
-    --size Standard_DS2_v2 \
     --nsg ${basename}-vm${subnet}_NSG \
     --public-ip-address ${basename}-vm${subnet}_PIP \
+    --size Standard_DS2_v2 \
     --generate-ssh-keys \
     --ssh-key-name vm${subnet}_ssh_key.pem
 done
 
+# Peering
+for i in az104-06-vnet2 az104-06-vnet3
+do
+az network vnet peering create \
+   -g az104-06-rg1 \
+   -n az104-06-vnet01To${i} \
+   --vnet-name az104-06-vnet01 \
+   --remote-vnet $i \
+   --allow-vnet-access
+done
 
 ## Generate pips in rg4 and rg5
+for i in {4..5}
+do
+az network public-ip create -g ${basename}-rg${i} -n ${basename}-pip${i}
+done
+
+#Generate lb4 in rg4
 
 
-
-# Task 2 and Task 3: Peering
-az network vnet peering create --name
-                               --remote-vnet
-                               --resource-group
-                               --vnet-name
-                               [--allow-forwarded-traffic]
-                               [--allow-gateway-transit]
-                               [--allow-vnet-access]
-                               [--use-remote-gateways]
+#Generate appgw5 in rg5
 
 
-# Task 4: Roles
-
-
-
-# Task 5: Generate lb4 in rg4
-
-
-
-# Task 6: Generate appgw5 in rg5
-
+##################################
+# Cleaning if neccessary
+# for i in az104-06-rg1 az104-06-rg4 az104-06-rg5
+# do
+#   az group delete --name $i
+# done
 
 
