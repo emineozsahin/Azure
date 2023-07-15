0ccf8ded48dbe7de9cb36b7962e7699e96806537
emineozsahin <54634534+emineozsahin@users.noreply.github.com>
2023-07-15 18:36:38 -0400

    updates

--- Azure_Implement_Traffic_Management/main.tf
+++ Azure_Implement_Traffic_Management/main.tf
@@ -24,15 +24,11 @@ resource "azurerm_resource_group" "terraform_sample" {
 # Variables
 variable "arm_region" {
   description = "The Azure region to create things in."
-  default     = "East US"
-}
-
-variable "arm_vm_admin_password" {
-  description = "Passwords for the root user in VMs."
-  default     = "SUper.123-" # This should be hidden and passed as variable, doing this just for training purpose
+  default     = "Canada Central"
 }
 
 variable "arm_frontend_instances" {
   description = "Number of front instances"
   default     = 2
 }
+
