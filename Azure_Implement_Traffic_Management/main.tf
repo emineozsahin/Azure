296254dfef96def752f2c74af9948d90551e0d1e
emineozsahin <54634534+emineozsahin@users.noreply.github.com>
2023-07-15 17:34:12 -0400

    updates

--- Azure_Implement_Traffic_Management/main.tf
+++ Azure_Implement_Traffic_Management/main.tf
@@ -15,7 +15,24 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
+# Resouce Group
+resource "azurerm_resource_group" "terraform_sample" {
+    name     = "traffic"
+    location = "${var.arm_region}"
+}
+
+# Variables
+variable "arm_region" {
+  description = "The Azure region to create things in."
+  default     = "East US"
+}
+
+variable "arm_vm_admin_password" {
+  description = "Passwords for the root user in VMs."
+  default     = "SUper.123-" # This should be hidden and passed as variable, doing this just for training purpose
+}
+
+variable "arm_frontend_instances" {
+  description = "Number of front instances"
+  default     = 2
 }
--- Azure_Implement_Traffic_Management/output.tf
+++ Azure_Implement_Traffic_Management/output.tf
@@ -4,3 +4,8 @@ output "frontend_id" {
 output "backend_id" {
   value = "${azurerm_subnet.my_subnet_backend.id}"
 }
+
+output "dmz_id" {
+  value = "${azurerm_subnet.my_subnet_dmz.id}"
+}
+
